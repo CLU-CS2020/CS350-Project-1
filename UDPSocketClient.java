ec9dcf8a01d7f582648b3894a6389dec64141920
ryan181 <ryan.mason814@gmail.com>
2018-06-25 23:17:49 -0700

    Update UDPSocketClient.java
    Added additional updates to Client switch case for the outgoing message to interact with Client's toString. Added switch/conditional's to Incoming message to interpret and act accordingly on. 
    deleted pseudo code. 
    added joke.

--- UDPSocketClient.java
+++ UDPSocketClient.java
@@ -9,15 +9,10 @@ import java.io.ObjectOutputStream;
 import java.net.*;
 import java.util.Scanner;
 
-/* simplex-talk server, UDP version */
-public class UDPSocketClient {
 
-    /**
-     * DatagramSocket: sending or receiving point for a packet delivery service.
-     * Each packet sent or received on a datagram socket is individually
-     * addressed and routed. Multiple packets sent from one machine to another
-     * may be routed differently, and may arrive in any order.
-     */
+public class UDPSocketClient  extends Client {
+
+
     DatagramSocket Socket;
 
     public UDPSocketClient() {
@@ -27,15 +22,9 @@ public class UDPSocketClient {
 
         try {
             Scanner input = new Scanner(System.in);
-            int AccountNumber; // 9 digit integer
-            int pin; // 4 numbers
-            int Request; // used for telling server what action to take
-            int amount; // amount of $ to deposit/ withdrawl
-            int balance;
-
             Socket = new DatagramSocket();
 
-            InetAddress IPAddress = InetAddress.getByName("10.100.2.1");
+            InetAddress IPAddress = InetAddress.getByName("localhost");
             // IP address of server destination. 
 
             System.out.println("welcome! what would you like to do? (Please enter in integer units only)"
@@ -45,104 +34,117 @@ public class UDPSocketClient {
                 Request = input.nextInt();
                 //0 = login,  1 = balance , 2 = withdrawl, 3 = deposit , 4 exit
 
-                Client client = null; // initializes client
+                Client client = new Client(); // initializes client
 
                 switch (Request) {
                     case 0: // login command
+                        client.setS_Request("Login"); 
                         System.out.println("enter account number");
                         AccountNumber = input.nextInt();
                         System.out.println("enter account pin");
                         pin = input.nextInt();
                         client = new Client(AccountNumber, pin);
+                        System.out.println(client.toString());
                         break;
 
                     case 1: // 1 = balance
+                        client.setS_Request("Balance"); 
                         System.out.println("asking server for balance");
                         client = new Client(1, -1, -1, -1);
+                        System.out.println(client.toString());
                         break;
 
                     case 2: // 2 = withdrawl
+                        client.setS_Request("Withdrawl"); 
                         System.out.println("How much would you like to withdrawl?");
                         amount = input.nextInt();
                         System.out.println("Withdrawing $" + amount + " dollars.");
                         client = new Client(2, -1, -1, amount);
+                        System.out.println(client.toString());
                         break;
 
                     case 3: // 3 = deposit
+                        client.setS_Request("Deposit"); 
                         System.out.println("How much would you like to deposit?");
                         amount = input.nextInt();
-                        System.out.println("Depositing $" + amount + "dollars into your account.");
+                        System.out.println("Requesting  $" + amount + "dollars to be deposited into your account.");
                         client = new Client(3, -1, -1, amount);
+                        System.out.println(client.toString());
                         break;
 
                     case 4: // 4 = exit
-                        System.out.println("would you like to exit? (Y/N)");
-                        if (input.next().toLowerCase() == "y") {
-                            client = new Client(4, -1, -1, -1);
-                            System.out.println("Goodbye!");
-                            System.exit(4);
-                            break;
-                        } else {
-                            System.out.println("What would you like to do?"
-                                    + "\n 0 = login,  1 = balance , 2 = withdrawl, 3 = deposit , 4 exit");
-                        }
+                        client.setS_Request("Exit"); 
+                        client = new Client(4, -1, -1, -1);
+                        System.out.println("Goodbye!");
+                        System.out.println(client.toString());
                         break;
+
                     default: // edge cases
                         System.out.println("invalid selection. Please try again.");
                         break;
                 }
+                
                 /* below is to send the client object*/
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                // This class implements an output stream in which the data is written into a byte array. 
-                //The buffer automatically grows as data is written to it. The data can be retrieved using toByteArray() and toString().
-
                 ObjectOutputStream os = new ObjectOutputStream(outputStream);
-                // writes primitive data types and graphs of Java objects to an OutputStream. The objects can 
-                //be read (reconstituted) using an ObjectInputStream. Persistent storage of objects can be 
-                //accomplished by using a file for the stream. If the stream is a network socket stream, the 
-                //objects can be reconstituted on another host or in another process.
-
+               
                 os.writeObject(client);
-                // writes new client object to send message
+            
 
                 byte[] b = outputStream.toByteArray();
-                // method creates a newly allocated buffer with the size as the current size of this output stream.
+             
 
                 DatagramPacket msg = new DatagramPacket(b, b.length, IPAddress, 4445);
-                // creates new datagram to send with coordinates
-                Socket.send(msg); // sends message
+       
+                Socket.send(msg);
 
-                // below is the actual receive operation
                 Socket.receive(msg);
                 System.err.println("message from <"
                         + msg.getAddress().getHostAddress() + "," + msg.getPort() + ">");
 
                 byte[] data = msg.getData();
-                // puts bytes in array through thr datagram packet to retrieve bytes ( b ) 
-
+               
                 ByteArrayInputStream in = new ByteArrayInputStream(data);
-                // places bytes into datastream to re-constitute message
+   
 
                 ObjectInputStream is = new ObjectInputStream(in);
-                // deserializes primitive data and objects previously written using an ObjectOutputStream.
+             
 
-                Client atmMessage = (Client) is.readObject(); // reads message
-                System.out.println("Message received :: " + atmMessage); // prints message
                 
+                Client atmMessage = (Client) is.readObject(); // reads message
+                System.out.println("Message received "); // prints message
+                switch (atmMessage.getRequest()) {
+                    case 5:
+                        if(Request == 0){ 
+                            System.out.println("Sucessfully logged in. " + " \n0 = login,  1 = balance , 2 = withdrawl, 3 = deposit , 4 exit ");
+                        }
+                        else if (Request == 1){ // balance check
+                            System.out.println("Sucessful: Account balance is " + atmMessage.getAmount() + ".");
+                            atmMessage.setBalance(atmMessage.getAmount());
+                        }else if ( Request == 2){ // withdrawl
+                            System.out.println("Sucessful: withdrawing " + amount + ". Account balance is " + atmMessage.getAmount() + ".");
+                            atmMessage.setBalance(atmMessage.getAmount());
+                        } else if (Request == 3) { // deposit
+                            System.out.println("Sucessful: depositing " + amount + ". Account balance is " + atmMessage.getAmount() + ".");
+                            atmMessage.setBalance(atmMessage.getAmount());
+                        }else{ System.out.println("Sucessfully.... broke the server? [PARADOX] ( Line144) ");  }
+                        break;
+                        
+                    case 6:
+                        System.out.println("There was an error with your Request. Please try again!");
+                        break;
+//                    case 4: // unknown if direction is to recieve response from server. 
+//                        System.out.println("Disconnected from Server");
+//                        System.exit(4); 
+                    default:
+                        System.out.println("How did you manage to get here?? [Line154]");
+                        break;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-//    /* 
-//    PSUDO:
-//    if (atmMessage(0) == 5)
-//    { print " balance/withdrawl/deposit request was a success." }
-//    else if (atmMessage(0) == 6) 
-//    { print ("error occured. please try again.")
-//     */
-//    public void ServerResponse() {
-//        
-//    }
+
 }
